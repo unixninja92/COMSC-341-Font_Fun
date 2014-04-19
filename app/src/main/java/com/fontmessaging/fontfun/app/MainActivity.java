02bb9d5cf448f218fa84e8db17d1ea9bf2737993
unixninja92 <charlesvt@gmail.com>
2014-04-18 21:21:29 -0400

    Made Font add button lead to font edit screen

--- app/src/main/java/com/fontmessaging/fontfun/app/MainActivity.java
+++ app/src/main/java/com/fontmessaging/fontfun/app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fontmessaging.fontfun.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +13,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createFont(View view) {
+        Intent draw = new Intent(this, DrawingActivity.class);
+        startActivity(draw);
     }
 }
 
